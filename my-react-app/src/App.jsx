f9c2d8b5190fbd473c97ddf1cc93cc5878353252
arif123 <nashrullah2344@gmail.com>
2024-01-23 20:38:01 +0700

    Create App.jsx

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { routes } from "./routes/routes";
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <Routes>
+      {routes.map((route, i) => (
+        <Route {...route} key={i} />
+      ))}
+    </Routes>
+  );
+}
+
+export default App;
